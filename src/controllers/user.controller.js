655bd468d208a8b3f75a44a17cfdcd4ff72ec19e
Santosh Patil <santoshpatil0000@gmail.com>
2025-12-29 12:04:46 +0530

    Fix token generation in login and refresh functions; standardize variable names for clarity

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -148,7 +148,7 @@ const loginUser = asyncHandler(async (req, res) => {
     }
 
     const { accessToken, refreshToken } = await generateAccessAndRefereshTokens(user._id)
-
+    
     const loggedInUser = await User.findById(user._id).select("-password -refreshToken")    // remove password and refresh token field from response using mongoose select method
 
     // above "loggedInUser" can also be created without making DB call,
@@ -237,16 +237,16 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
             secure: true
         }
 
-        const { accessToken, newRefreshToken } = await generateAccessAndRefereshTokens(user._id)
+        const { accessToken, refreshToken } = await generateAccessAndRefereshTokens(user._id)        
 
         return res
             .status(200)
             .cookie("accessToken", accessToken, options)
-            .cookie("refreshToken", newRefreshToken, options)
+            .cookie("refreshToken", refreshToken, options)
             .json(
                 new ApiResponse(
                     200,
-                    { accessToken, refreshToken: newRefreshToken },
+                    { accessToken, refreshToken },
                     "Access token refreshed"
                 )
             )
